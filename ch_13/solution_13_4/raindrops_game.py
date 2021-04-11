41db0770b7d60b79a18bf7b47025c9a2301b4ecc
Eric Matthes <eric@Erics-MacBook-Pro.local>
2021-04-10 16:54:54 -0800

    Started Ex 13-4.

---
+++ ch_13/solution_13_4/raindrops_game.py
@@ -0,0 +1,105 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from raindrop import Raindrop
+
+class RaindropsGame:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Raindrops")
+
+        self.raindrops = pygame.sprite.Group()
+        self._create_drops()
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_raindrops()
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    def _create_drops(self):
+        """Fill the sky with raindrops."""
+        # Create an drop and find the number of drops in a row.
+        # Spacing between each drop is equal to one drop width.
+        #   Note that the spacing here works reasonably for larger drops.
+        #   If you're working with smaller drops, there might be a better
+        #   approach to spacing.
+        drop = Raindrop(self)
+        drop_width, drop_height = drop.rect.size
+        available_space_x = self.settings.screen_width - drop_width
+        number_drops_x = available_space_x // (2 * drop_width)
+        
+        # Determine the number of rows of drops that fit on the screen.
+        available_space_y = self.settings.screen_height
+        number_rows = available_space_y // (2 * drop_height)
+        
+        # Fill the sky with drops.
+        for row_number in range(number_rows):
+            for drop_number in range(number_drops_x):
+                self._create_drop(drop_number, row_number)
+
+    def _create_drop(self, drop_number, row_number):
+        """Create an drop and place it in the row."""
+        drop = Raindrop(self)
+        drop_width, drop_height = drop.rect.size
+        drop.rect.x = drop_width + 2 * drop_width * drop_number
+        drop.y = 2 * drop.rect.height * row_number
+        drop.rect.y = drop.y
+        self.raindrops.add(drop)
+
+    def _update_raindrops(self):
+        """Update drop positions, and look for drops
+        that have disappeared.
+        """
+        self.raindrops.update()
+
+        # Assume we won't make new drops.
+        make_new_drops = False
+        for drop in self.raindrops.copy():
+            if drop.check_disappeared():
+                # Remove this drop, and we'll need to make new drops.
+                self.raindrops.remove(drop)
+                make_new_drops = True
+
+        # Make a new row of drops if needed.
+        if make_new_drops:
+            print('making new drops')
+            pass
+
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.raindrops.draw(self.screen)
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    rd_game = RaindropsGame()
+    rd_game.run_game()
