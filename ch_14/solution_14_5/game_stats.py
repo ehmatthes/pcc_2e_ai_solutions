dd6d8a19a4a8365c3226f5a9fcac0d83b4d6b956
Eric Matthes <eric@Erics-MacBook-Pro.local>
2021-04-13 12:47:29 -0800

    Tries to read high score from file.

--- ch_14/solution_14_5/game_stats.py
+++ ch_14/solution_14_5/game_stats.py
@@ -10,8 +10,16 @@ class GameStats:
         self.game_active = False
 
         # High score should never be reset.
-        self.high_score = 0
-        
+        self._get_high_score()
+
+    def _get_high_score(self):
+        """Gets high score from file, if it exists."""
+        try:
+            with open('high_score.json') as f:
+                self.high_score = json.load(f)
+        except FileNotFoundError:
+            self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
